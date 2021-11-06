99bccafe251ab6c34812a06363947e9f67ffba9b
summaru <dkstjd5183@gmail.com>
2021-11-06 13:06:46 +0900

    dont use default, becase javascript export default read  with var

--- src/lib.ts
+++ src/lib.ts
@@ -4,21 +4,21 @@ import {login} from './handler/login';
 import m from './handler/menu';
 
 
-export default class {
+class l{
     private isUseMiddleware = false;
 
-    public initDB(host : string,port : number,
-        userName : string,password : string,dbName : string) {
+    public initDB = (host : string,port : number,
+        userName : string,password : string,dbName : string) => {
         init(host,port,userName,password,dbName).catch((reason)=>{
             throw reason;
         });
     }
-    public useMiddleware(app : express.Express) {
+    public useMiddleware = (app : express.Express) => {
         app.use(express.json());
         app.use(express.urlencoded());
         this.isUseMiddleware = true;
     }
-    public mappingApiRouter(router : express.Router) {
+    public mappingApiRouter = (router : express.Router) => {
         if (!this.isUseMiddleware)
             throw "call useMiddleware method,when before call mappingApiRouter";
         router.use("/login",login);
@@ -27,4 +27,11 @@ export default class {
         router.use("/delete_item",m.privateHandler.delete);
         router.use("/create_item",m.privateHandler.create);
     }
+
+    public constructor() {}
 }
+const api = new l();
+
+export const mappingApiRouter = api.mappingApiRouter;
+export const useMiddleware = api.useMiddleware;
+export const initDB = api.initDB;
