9666086a30f9f8bb3731dc94e400b568f88c34a4
summaru <dkstjd5183@gmail.com>
2021-11-07 21:04:17 +0900

    change

--- src/var_box.ts
+++ src/var_box.ts
@@ -2,8 +2,9 @@ import mysql2 from 'mysql2/promise';
 
 
 let conn : mysql2.Connection;
+let staticPath : string;
 
-export async function init(host : string,port : number,
+export async function initDB(host : string,port : number,
     userName : string,password : string,dbName : string) {
     conn =await mysql2.createConnection({
         host : host,
@@ -13,6 +14,14 @@ export async function init(host : string,port : number,
         database : dbName
     });
 }
+
+export function initAdminFolder(path : string) {
+    staticPath = path;
+}
+
+export function getAdminStatic() : string {
+    return staticPath;
+} 
 
 export function getConn() : mysql2.Connection {
     return conn;
