9f421cb1af3225bdb360cf971aae85056024ef88
summaru <dkstjd5183@gmail.com>
2021-11-07 16:57:08 +0900

    page project init

---
+++ page/index.tsx
@@ -0,0 +1,10 @@
+// index.tsx
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src/App';
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>, 
+  document.getElementById('root'),
+);
---
+++ page/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {parseCookies, setCookie} from 'nookies';
+
+
+
+
+class App extends React.Component{
+    private sessionKey : string = "";
+    public constructor({}) {
+        super({});
+    }
+
+
+    public render() {
+        return (
+            <div>
+               
+            </div>
+        );
+    }
+}
+
+
+
+export default App;
