ffaf52fee23ef38cf314dc47a76d8aa34e6567c1
summaru <dkstjd5183@gmail.com>
2021-11-07 21:04:54 +0900

    get init list

--- src/lib.ts
+++ src/lib.ts
@@ -1,32 +1,52 @@
 import express from 'express';
-import {init} from './db';
+import * as init from './var_box';
 import {login} from './handler/login';
 import m from './handler/menu';
 
 
-class l{
-    private isUseMiddleware = false;
+interface InitList {
+    db : boolean
+    middleware : boolean
+    staticPage : boolean
+    router : boolean
+}
 
+class l{
+    private list : InitList =  {db :false, middleware : false, staticPage : false,router :false};
     public initDB = (host : string,port : number,
         userName : string,password : string,dbName : string) => {
-        init(host,port,userName,password,dbName).catch((reason)=>{
+        init.initDB(host,port,userName,password,dbName).catch((reason)=>{
             throw reason;
+        }).then(() => {
+            this.list.db = true;
         });
     }
+
     public useMiddleware = (app : express.Express) => {
         app.use(express.json());
         app.use(express.urlencoded());
-        this.isUseMiddleware = true;
+
+        this.list.middleware = true;
     }
+
+    public setAdminStaticPage = (app : express.Express,path : string) => {
+        init.initAdminFolder(path);
+        app.use(express.static(path))
+        this.list.staticPage = true;
+    }
+
     public mappingApiRouter = (router : express.Router) => {
-        if (!this.isUseMiddleware)
-            throw "call useMiddleware method,when before call mappingApiRouter";
         router.use("/login",m.publicHandler.adminLogin);
         router.use("/chk_session",m.publicHandler.sessionCheck);
         
         router.use("/get_menu",m.publicHandler.getList);
         router.use("/delete_item",m.privateHandler.delete);
         router.use("/create_item",m.privateHandler.create);
+        this.list.router = true;
+    }
+
+    public initList = () => {
+        return this.list;
     }
 
     public constructor() {}
